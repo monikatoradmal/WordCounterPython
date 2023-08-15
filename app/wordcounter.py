e7e9d2083e22b86ba6e018d33161b97e79fbe1c6
Monika Toradmal <monika.toradmal22@gmail.com>
2023-08-15 17:46:04 +0100

    added dockerfile

--- app/wordcounter.py
+++ app/wordcounter.py
@@ -16,12 +16,8 @@ def get_count_by_word(word:str):
     if valid_words.get(word) != None:
         return {word : valid_words.get(word)}    
     raise HTTPException(status_code=404,detail="Word not found")
-        
-def check_English_Dictionary(word):
-    #This is a dummy method to check whether passed word is english or not  
-    return word 
-
   
+
 @app.post('/words/add')
 def add_words(words :List[str]):
     for word in words:
@@ -39,8 +35,11 @@ def add_words(words :List[str]):
             valid_words.update({word:1})    
     
     return "Words added successfully"
+
     
-    
+def check_English_Dictionary(word):
+    #This is a dummy method to check whether passed word is english or not  
+    return word 
    
 
          
